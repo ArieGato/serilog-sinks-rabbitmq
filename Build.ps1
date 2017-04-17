27c187c392aa7d05a4ce35bd026bfa8ab195e4af
Jared Nance <jaredcnance@gmail.com>
2017-04-17 13:07:39 -0500

    debug appveyor build

--- Build.ps1
+++ Build.ps1
@@ -6,4 +6,7 @@ dotnet build .\src\Serilog.Sinks.RabbitMQ\Serilog.Sinks.RabbitMQ.csproj
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
+echo "RUNNING dotnet pack .\src\Serilog.Sinks.RabbitMQ\Serilog.Sinks.RabbitMQ.csproj -c Release -o .\artifacts --version-suffix=$revision"
 dotnet pack .\src\Serilog.Sinks.RabbitMQ\Serilog.Sinks.RabbitMQ.csproj -c Release -o .\artifacts --version-suffix=$revision
+
+dir
