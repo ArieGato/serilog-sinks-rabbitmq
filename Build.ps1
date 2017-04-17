94a5b3cc12e03ace5293267871e13015744ae79f
Jared Nance <jaredcnance@gmail.com>
2017-04-17 13:28:42 -0500

    remove version suffix
    since pre-releases are never published

--- Build.ps1
+++ Build.ps1
@@ -3,10 +3,5 @@ if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 dotnet restore .\src\Serilog.Sinks.RabbitMQ\Serilog.Sinks.RabbitMQ.csproj
 dotnet build .\src\Serilog.Sinks.RabbitMQ\Serilog.Sinks.RabbitMQ.csproj
 
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
-
-echo "RUNNING dotnet pack .\src\Serilog.Sinks.RabbitMQ\Serilog.Sinks.RabbitMQ.csproj -c Release -o .\artifacts --version-suffix=$revision"
-dotnet pack .\src\Serilog.Sinks.RabbitMQ\Serilog.Sinks.RabbitMQ.csproj -c Release -o .\artifacts --version-suffix=$revision
-
-dir
+echo "RUNNING dotnet pack .\src\Serilog.Sinks.RabbitMQ\Serilog.Sinks.RabbitMQ.csproj -c Release -o .\artifacts"
+dotnet pack .\src\Serilog.Sinks.RabbitMQ\Serilog.Sinks.RabbitMQ.csproj -c Release -o .\artifacts
