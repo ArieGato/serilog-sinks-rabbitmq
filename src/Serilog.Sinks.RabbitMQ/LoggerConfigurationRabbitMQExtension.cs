431bfdfb061c925863a4195c95af0716ad70840a
Juris Gekiss <gekiss@inbox.lv>
2018-10-31 14:57:35 +0200

    Added support for AuditTo

--- src/Serilog.Sinks.RabbitMQ/LoggerConfigurationRabbitMQExtension.cs
+++ src/Serilog.Sinks.RabbitMQ/LoggerConfigurationRabbitMQExtension.cs
@@ -108,6 +108,51 @@ namespace Serilog
                     .Sink(new RabbitMQSink(config, formatter, formatProvider));
         }
 
+        /// <summary>
+        /// Configures Serilog audit logger configuration with RabbitMQ
+        /// </summary>
+        public static LoggerConfiguration RabbitMQ(
+            this LoggerAuditSinkConfiguration loggerAuditSinkConfiguration,
+            string hostname,
+            string username,
+            string password,
+            string exchange = null,
+            string exchangeType = null,
+            RabbitMQDeliveryMode deliveryMode = RabbitMQDeliveryMode.NonDurable,
+            string routeKey = null,
+            int port = 0,
+            string vHost = null,
+            ushort heartbeat = 0,
+            IProtocol protocol = null,
+            ITextFormatter formatter = null,
+            IFormatProvider formatProvider = null) {
+            // guards
+            if (loggerAuditSinkConfiguration == null) throw new ArgumentNullException("loggerAuditSinkConfiguration");
+            if (string.IsNullOrEmpty(hostname)) throw new ArgumentException("hostname cannot be 'null'. Enter a valid hostname.");
+            if (string.IsNullOrEmpty(username)) throw new ArgumentException("username cannot be 'null' or and empty string.");
+            if (password == null) throw new ArgumentException("password cannot be 'null'. Specify an empty string if password is empty.");
+            if (port < 0 || port > 65535) throw new ArgumentOutOfRangeException("port", "port must be in a valid range (1 and 65535 or 0 for default)");
+
+            // setup configuration
+            var config = new RabbitMQConfiguration {
+                Hostname = hostname,
+                Username = username,
+                Password = password,
+                Exchange = exchange ?? string.Empty,
+                ExchangeType = exchangeType ?? string.Empty,
+                DeliveryMode = deliveryMode,
+                RouteKey = routeKey ?? string.Empty,
+                Port = port,
+                VHost = vHost ?? string.Empty,
+                Protocol = protocol ?? Protocols.DefaultProtocol,
+                Heartbeat = heartbeat,
+            };
+
+            return
+                loggerAuditSinkConfiguration
+                    .Sink(new RabbitMQAuditSink(config, formatter, formatProvider));
+        }
+
         private const int DefaultBatchPostingLimit = 50;
         private static readonly TimeSpan DefaultPeriod = TimeSpan.FromSeconds(2);
     }
---
+++ src/Serilog.Sinks.RabbitMQ/Sinks/RabbitMQ/RabbitMQAuditSink.cs
@@ -0,0 +1,66 @@
+﻿using Serilog.Core;
+using Serilog.Debugging;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Raw;
+using Serilog.Sinks.RabbitMQ.Sinks.RabbitMQ;
+using System;
+using System.IO;
+
+namespace Serilog.Sinks.RabbitMQ
+{
+    // <summary>
+    /// Serilog RabbitMq Sink - Lets you log to RabbitMq using Serilog audit logic
+    /// </summary>
+    public class RabbitMQAuditSink : ILogEventSink, IDisposable {
+        private readonly ITextFormatter _formatter;
+        private readonly IFormatProvider _formatProvider;
+        private readonly RabbitMQClient _client;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RabbitMQAuditSink"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="formatter">The formatter, or null.</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        public RabbitMQAuditSink(RabbitMQConfiguration configuration, ITextFormatter formatter, IFormatProvider formatProvider) {
+            _formatter = formatter ?? new RawFormatter();
+            _formatProvider = formatProvider;
+            _client = new RabbitMQClient(configuration);
+        }
+
+        /// <summary>
+        /// Emit the provided log event to the sink.
+        /// </summary>
+        /// <param name="logEvent">The log event to write.</param>
+        public void Emit(LogEvent logEvent) {
+            try {
+                var sw = new StringWriter();
+                _formatter.Format(logEvent, sw);
+                _client.Publish(sw.ToString());
+            }
+            catch (Exception ex) {
+                SelfLog.WriteLine("Unable to write log event to the RabbitMQ due to following error: {1}", ex.Message);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing) {
+            if (disposing) {
+                _client.Dispose();
+            }
+        }
+    }
+}
