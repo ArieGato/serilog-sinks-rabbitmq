6aaeb8c85587f8a11f3f8a6b3e5c07d374e0bb0e
Arjan Vermunt <a.vermunt@dutchwebsolutions.nl>
2024-11-15 13:02:27 +0100

    Removed link

--- src/Serilog.Sinks.RabbitMQ/Sinks/RabbitMQ/AsyncHelpers.cs
+++ src/Serilog.Sinks.RabbitMQ/Sinks/RabbitMQ/AsyncHelpers.cs
@@ -13,11 +13,10 @@
 // limitations under the License.
 //
 // Adapted from Rebus
-//
-// source: https://github.com/restsharp/RestSharp/tree/dev/src/RestSharp
 
 using System.Collections.Concurrent;
 using System.Runtime.ExceptionServices;
+
 #pragma warning disable SA1513
 #pragma warning disable SA1313
 #pragma warning disable SA1500
@@ -30,20 +29,24 @@ using System.Runtime.ExceptionServices;
 
 namespace Serilog.Sinks.RabbitMQ;
 
-static class AsyncHelpers {
+internal static class AsyncHelpers
+{
     /// <summary>
     /// Executes a task synchronously on the calling thread by installing a temporary synchronization context that queues continuations
     /// </summary>
     /// <param name="task">Callback for asynchronous task to run</param>
-    public static void RunSync(Func<Task> task) {
+    public static void RunSync(Func<Task> task)
+    {
         var currentContext = SynchronizationContext.Current;
-        var customContext  = new CustomSynchronizationContext(task);
+        var customContext = new CustomSynchronizationContext(task);
 
-        try {
+        try
+        {
             SynchronizationContext.SetSynchronizationContext(customContext);
             customContext.Run();
         }
-        finally {
+        finally
+        {
             SynchronizationContext.SetSynchronizationContext(currentContext);
         }
     }
@@ -54,7 +57,8 @@ static class AsyncHelpers {
     /// <param name="task">Callback for asynchronous task to run</param>
     /// <typeparam name="T">Return type for the task</typeparam>
     /// <returns>Return value from the task</returns>
-    public static T RunSync<T>(Func<Task<T>> task) {
+    public static T RunSync<T>(Func<Task<T>> task)
+    {
         T result = default!;
         RunSync(async () => { result = await task(); });
         return result;
@@ -63,26 +67,31 @@ static class AsyncHelpers {
     /// <summary>
     /// Synchronization context that can be "pumped" in order to have it execute continuations posted back to it
     /// </summary>
-    class CustomSynchronizationContext : SynchronizationContext {
-        readonly ConcurrentQueue<Tuple<SendOrPostCallback, object?>> _items            = new();
-        readonly AutoResetEvent                                      _workItemsWaiting = new(false);
-        readonly Func<Task>                                          _task;
-        ExceptionDispatchInfo?                                       _caughtException;
-        bool                                                         _done;
+    private class CustomSynchronizationContext : SynchronizationContext
+    {
+        private readonly ConcurrentQueue<Tuple<SendOrPostCallback, object?>> _items = new();
+        private readonly AutoResetEvent _workItemsWaiting = new(false);
+        private readonly Func<Task> _task;
+        private ExceptionDispatchInfo? _caughtException;
+        private bool _done;
 
         /// <summary>
         /// Constructor for the custom context
         /// </summary>
         /// <param name="task">Task to execute</param>
-        public CustomSynchronizationContext(Func<Task> task) =>
-            _task = task ?? throw new ArgumentNullException(nameof(task), "Please remember to pass a Task to be executed");
+        public CustomSynchronizationContext(Func<Task> task)
+        {
+            _task = task ??
+                    throw new ArgumentNullException(nameof(task), "Please remember to pass a Task to be executed");
+        }
 
         /// <summary>
         /// When overridden in a derived class, dispatches an asynchronous message to a synchronization context.
         /// </summary>
         /// <param name="function">Callback function</param>
         /// <param name="state">Callback state</param>
-        public override void Post(SendOrPostCallback function, object? state) {
+        public override void Post(SendOrPostCallback function, object? state)
+        {
             _items.Enqueue(Tuple.Create(function, state));
             _workItemsWaiting.Set();
         }
@@ -90,31 +99,41 @@ static class AsyncHelpers {
         /// <summary>
         /// Enqueues the function to be executed and executes all resulting continuations until it is completely done
         /// </summary>
-        public void Run() {
-            async void PostCallback(object? _) {
-                try {
+        public void Run()
+        {
+            async void PostCallback(object? _)
+            {
+                try
+                {
                     await _task().ConfigureAwait(false);
                 }
-                catch (Exception exception) {
+                catch (Exception exception)
+                {
                     _caughtException = ExceptionDispatchInfo.Capture(exception);
                     throw;
                 }
-                finally {
+                finally
+                {
                     Post(_ => _done = true, null);
                 }
             }
 
             Post(PostCallback, null);
 
-            while (!_done) {
-                if (_items.TryDequeue(out var task)) {
+            while (!_done)
+            {
+                if (_items.TryDequeue(out var task))
+                {
                     task.Item1(task.Item2);
-                    if (_caughtException == null) {
+                    if (_caughtException == null)
+                    {
                         continue;
                     }
+
                     _caughtException.Throw();
                 }
-                else {
+                else
+                {
                     _workItemsWaiting.WaitOne();
                 }
             }
@@ -125,7 +144,8 @@ static class AsyncHelpers {
         /// </summary>
         /// <param name="function">Callback function</param>
         /// <param name="state">Callback state</param>
-        public override void Send(SendOrPostCallback function, object? state) => throw new NotSupportedException("Cannot send to same thread");
+        public override void Send(SendOrPostCallback function, object? state) =>
+            throw new NotSupportedException("Cannot send to same thread");
 
         /// <summary>
         /// When overridden in a derived class, creates a copy of the synchronization context. Not needed, so just return ourselves.
